c48e987da85dd12557b76f70815ccbf1cbaf5e25
jaehoonchoi <eeoos8840@gmail.com>
2025-04-28 12:28:14 +0900

    Feat(StaticFileHandler): 로그인 상태 여부에 따라 html 수정
    - ContentType이 Html이고, 로그인 여부에 따라 버튼을 다르게 보이도록 수정
    - 로그인 상태라면 사용자 이름, 로그아웃 버튼
    - 로그인 상태가 아니라면 로그인, 회원가입 버튼

--- src/main/java/router/handler/impl/StaticFileHandler.java
+++ src/main/java/router/handler/impl/StaticFileHandler.java
@@ -1,5 +1,6 @@
 package router.handler.impl;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import router.handler.HttpRequestHandler;
@@ -7,8 +8,10 @@ import util.FileUtils;
 import webserver.common.ContentType;
 import webserver.http.request.HttpRequest;
 import webserver.http.response.HttpResponse;
+import webserver.http.session.HttpSession;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class StaticFileHandler implements HttpRequestHandler {
     private static final Logger logger = LoggerFactory.getLogger(StaticFileHandler.class);
@@ -25,10 +28,48 @@ public class StaticFileHandler implements HttpRequestHandler {
 
         try {
             byte[] body = FileUtils.readFileBytes(path);
+
+            if (contentType == ContentType.HTML) {
+                String content = new String(body, StandardCharsets.UTF_8);
+                content = processDynamicHtml(content, request);
+                body = content.getBytes(StandardCharsets.UTF_8);
+            }
+
             response.sendOk(contentType, body);
         } catch (IOException e) {
             logger.error("파일을 찾을 수 없습니다: {}", path);
             response.send404();
         }
+    }
+
+    private String processDynamicHtml(String content, HttpRequest request) {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+
+        if (user != null) {
+            String loggedInHeader =
+                    "<ul class=\"header__menu\">\n" +
+                            "          <li class=\"header__menu__item\">\n" +
+                            "            <span>안녕하세요, " + user.getName() + "님</span>\n" +
+                            "          </li>\n" +
+                            "          <li class=\"header__menu__item\">\n" +
+                            "<form action=\"/user/logout\" method=\"post\">\n" +
+                            "              <button type=\"submit\" class=\"btn btn_contained btn_size_s\">로그아웃</button>\n" +
+                            "            </form>" +
+                            "          </li>\n" +
+                            "        </ul>";
+
+            content = content.replaceAll("<ul class=\"header__menu\">\n" +
+                    "          <li class=\"header__menu__item\">\n" +
+                    "            <a class=\"btn btn_contained btn_size_s\" href=\"/login/index.html\">로그인</a>\n" +
+                    "          </li>\n" +
+                    "          <li class=\"header__menu__item\">\n" +
+                    "            <a class=\"btn btn_ghost btn_size_s\" href=\"/registration/index.html\">\n" +
+                    "              회원 가입\n" +
+                    "            </a>\n" +
+                    "          </li>\n" +
+                    "        </ul>", loggedInHeader);
+        }
+        return content;
     }
 }
